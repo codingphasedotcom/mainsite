fcdb0992782bf7d9a91d4757500bb128c3415376
Joe Santos Garcia <joesantosgarcia@gmail.com>
2017-06-14 19:03:59 -0400

    no message

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsers extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -31,7 +31,6 @@ class CreateUsers extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-        
     }
 
     /**
